2027cb135716b8cbbfabed3a15ba1882a4d4c160
JammyPajamies <24786411+EvanGuanSF@users.noreply.github.com>
2025-04-06 07:04:04 -0700

    Attempt fix for regex error
    Did not need to escape '/' characters esp. for Linux. Removed some unnecessary multiline tags from regex patterns.

--- modules/processing_scripts/comments.py
+++ modules/processing_scripts/comments.py
@@ -3,8 +3,15 @@ import re
 
 
 def strip_comments(text):
-    text = re.sub('(^|\n)#[^\n]*(\n|$)', '\n', text)  # while line comment
-    text = re.sub('#[^\n]*(\n|$)', '\n', text)  # in the middle of the line comment
+    text = re.sub('(^/\*.*?\*/(\n|$))(?m)(?s)', '\n', text)  # multiline comments (/* */)
+    text = re.sub('(^#.*(\n|$))', '\n', text)  # whole line comment (#)
+    text = re.sub('(^//.*(\n|$))', '\n', text)  # whole line comment (//)
+    text = re.sub('(#.*(\n|$))', '\n', text)  # middle of the line comment (#)
+    text = re.sub('(//.*(\n|$))', '\n', text)  # middle of the line comment (//)
+    text = re.sub('(/\*.*(\n|$))|(\*/.*(\n|$))', '\n', text)  # dangling multiline comment brackets (/* */)
+    #text = re.sub('[\n]{3,}', '\n\n', text)  # remove multiple consecutive newlines
+    #text = re.sub('[ ]{2,}', ' ', text)  # remove multiple consecutive spaces
+    #text.rstrip() # finally, strip leading and trailing whitespace
 
     return text
 
