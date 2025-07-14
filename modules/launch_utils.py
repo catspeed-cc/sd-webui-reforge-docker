90adc6d5f84b74398e4d07bbef80128a715033d8
panchovix <francisco.jaqueat@gmail.com>
2025-07-14 19:05:40 -0400

    Torch 2.7.1

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -369,7 +369,7 @@ def get_cuda_comp_cap():
 
 def prepare_environment():
     torch_index_url = os.environ.get('TORCH_INDEX_URL', "https://download.pytorch.org/whl/cu128")
-    torch_command = os.environ.get('TORCH_COMMAND', f"pip install torch==2.7.0 torchvision==0.22.0 --extra-index-url {torch_index_url}")
+    torch_command = os.environ.get('TORCH_COMMAND', f"pip install torch==2.7.1 torchvision==0.22.1 --extra-index-url {torch_index_url}")
     if args.use_ipex:
         if platform.system() == "Windows":
             # The "Nuullll/intel-extension-for-pytorch" wheels were built from IPEX source for Intel Arc GPU: https://github.com/intel/intel-extension-for-pytorch/tree/xpu-main
