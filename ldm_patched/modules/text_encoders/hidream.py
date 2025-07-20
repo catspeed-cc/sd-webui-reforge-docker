7cff299630a2b607d1dd4335da8657af88fbbd03
panchovix <francisco.jaqueat@gmail.com>
2025-07-19 21:23:45 -0400

    (N/A) Use empty t5 of size 128 for hidream, seems to give closer results. https://github.com/comfyanonymous/ComfyUI/commit/fd274944418f1148b762a6e2d37efa820a569071

--- ldm_patched/modules/text_encoders/hidream.py
+++ ldm_patched/modules/text_encoders/hidream.py
@@ -116,7 +116,7 @@ class HiDreamTEModel(torch.nn.Module):
             ll_out = ll_out[:, 1:]
 
         if t5_out is None:
-            t5_out = torch.zeros((1, 1, 4096), device=ldm_patched.modules.model_management.intermediate_device())
+            t5_out = torch.zeros((1, 128, 4096), device=ldm_patched.modules.model_management.intermediate_device())
 
         if ll_out is None:
             ll_out = torch.zeros((1, 32, 1, 4096), device=ldm_patched.modules.model_management.intermediate_device())
